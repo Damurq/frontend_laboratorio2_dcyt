d14024e609f65f7f88854f9c27d2a1df2eafda2c
Damurq <michaelmontero.idb@gmail.com>
2021-10-07 12:19:01 -0400

    update

--- src/ui-component/cards/AuthFooter.js
+++ src/ui-component/cards/AuthFooter.js
@@ -7,11 +7,11 @@ import { Link, Typography, Stack } from '@material-ui/core';
 
 const AuthFooter = () => (
     <Stack direction="row" justifyContent="space-between">
-        <Typography variant="subtitle2" component={Link} href="https://berrydashboard.io" target="_blank" underline="hover">
-            berrydashboard.io
+        <Typography variant="subtitle2" component={Link} href="#" target="_blank" underline="hover">
+            UCLA
         </Typography>
-        <Typography variant="subtitle2" component={Link} href="https://codedthemes.com" target="_blank" underline="hover">
-            &copy; codedthemes.com
+        <Typography variant="subtitle2" component={Link} href="#" target="_blank" underline="hover">
+            &copy; DCYT
         </Typography>
     </Stack>
 );
